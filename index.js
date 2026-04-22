3dc5489742bdf73e97f718b21f2bfd5b6db50ed8
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-04-22 13:23:08 +0200

    guardado dinamico y correcto

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ import multer from 'multer'
 
 const api=express()
 
-const uploads=multer({dest:'uploads/img'})
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/img')
+  },
+  filename: function (req, file, cb) {
+    console.log(file)
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.originalname)
+  }
+})
+
+const uploads=multer({ storage: storage })
 api.use(express.json())
 
 const port=3000
@@ -12,7 +23,7 @@ api.get('/', (req, res)=>{
     res.send("api conectada")
 })
 
-api.post('/profile', uploads.single('avatar'), function (req, res, next) {
+api.post('/profile', uploads.single('avatar'), (req, res)=>{
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
   res.send("archivo enviado")
